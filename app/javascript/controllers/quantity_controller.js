b1500dd3dc900e92849ccf00e64b097dacf6f6c0
Oceane Feltrin <oceanefeltrin@yahoo.fr>
2022-08-07 14:22:01 +0200

    js for price done

---
+++ app/javascript/controllers/quantity_controller.js
@@ -0,0 +1,26 @@
+const quantity = document.getElementById('quantity')
+const qPLus = document.getElementById('plus')
+const qMinus = document.getElementById('minus')
+const price = document.getElementById('price')
+
+qPLus.addEventListener('click', () => {
+  newQuantityPlus =  parseInt(quantity.innerText) + 1
+  quantity.innerText = newQuantityPlus
+  // newPricePlus = parseInt(quantity.innerText) * 1.20
+  // price.innerText = newPricePlus.toFixed(2)
+})
+
+qMinus.addEventListener('click', () => {
+  newQuantityMinus = parseInt(quantity.innerText) - 1
+  if (quantity.innerText >= 1) {
+    quantity.innerText = newQuantityMinus
+  } else {
+    quantity.innerText = '1'
+  }
+  // newPriceMinus = parseInt(quantity.innerText) / 1.20
+  // if (newQuantityMinus <= 0) {
+  //   price.innerText = '0'
+  // } else {
+  //   price.innerText = newPriceMinus.toFixed(2)
+  // }
+})
