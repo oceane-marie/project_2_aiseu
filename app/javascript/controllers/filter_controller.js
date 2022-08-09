157d73ce3ef115f9a8028d5a9067265663330b9a
Oceane Feltrin <oceanefeltrin@yahoo.fr>
2022-08-09 16:40:50 +0200

    js fetch with checkboxes is working wow!!!

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -6,56 +6,36 @@ export default class extends Controller {
   static targets = [ "list", "tempcheckbox", 'flavorcheckbox'];
 
   connect() {
+    console.log(this.element)
     console.log(this.tempcheckboxTargets)
     console.log(this.flavorcheckboxTargets)
-    console.log(this.element)
   }
 
   sortbytemp(event) {
     event.preventDefault()
-    // this.tempcheckboxTargets.forEach(() => {
-      if (event.target.checked) {
-        event.currentTarget.setAttribute('checked', 'checked')
-        if (event.currentTarget.checked = 'checked') {
-          const url = `/?temp=${event.currentTarget.value}`
-          console.log(event.currentTarget.value)
+    this.tempcheckboxTargets.forEach(() => {
+      const url = `/?temp=${event.target.innerText}`
+          console.log(event.target.innerText)
           console.log(url)
           fetch(url, { headers: { 'Accept': 'text/plain' } })
           .then(response => response.text())
           .then((data) => {
           this.listTarget.outerHTML = data;
-        })
-        }
-      }
-    // })
-
-    // this.tempcheckboxTargets.forEach((tempcheckbox) => {
-    //   if (tempcheckbox.checked = 'checked') {
-    //   const url = `/?temp=${tempcheckbox.value}`
-    //   console.log(tempcheckbox.value)
-    //   console.log(url)
-    //   fetch(url, { headers: { 'Accept': 'text/plain' } })
-    //     .then(response => response.text())
-    //     .then((data) => {
-    //     this.listTarget.outerHTML = data;
-    //   })
-    // }
-    // })
+          })
+    })
   }
 
   sortbyflavor(event) {
     event.preventDefault()
-    this.flavorcheckboxTargets.forEach((flavorcheckbox) => {
-      if (flavorcheckbox.checked = 'checked') {
-        console.log(flavorcheckbox.value)
-        const url = `/?flavor=${flavorcheckbox.value}`
-        console.log(url)
-        fetch(url, { headers: { 'Accept': 'text/plain' } })
+    this.flavorcheckboxTargets.forEach(() => {
+      const url = `/?flavor=${event.target.innerText}`
+          console.log(event.target.innerText)
+          console.log(url)
+          fetch(url, { headers: { 'Accept': 'text/plain' } })
           .then(response => response.text())
           .then((data) => {
           this.listTarget.outerHTML = data;
-        })
-      }
+          })
     })
   }
 }
