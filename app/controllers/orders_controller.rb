b8492eb8b628b5d786a003c0c2cc3be84ac19012
Oceane Feltrin <oceanefeltrin@yahoo.fr>
2022-08-12 18:12:22 +0200

    session count added

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,16 +2,20 @@ class OrdersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    if user_signed_in?
-      @order = Order.where(user_id: current_user)
-    else
-      if session[:order]
-        @order = Order.find(session[:order])
-        # @order_items = @order.where(order_item_id: @order_item)
-      else
-        @order = Order.create
-        session[:order] = @order.id
-      end
-    end
+    session[:visit_count] ||= 0 # initialize
+    session[:visit_count] += 1 # increment
+    @visit_count = session[:visit_count]
+
+    # if user_signed_in?
+    #   @order = Order.where(user_id: current_user)
+    # else
+    #   if session[:order]
+    #     @order = Order.find(session[:order])
+    #     # @order_items = @order.where(order_item_id: @order_item)
+    #   else
+    #     @order = Order.create
+    #     session[:order] = @order.id
+    #   end
+    # end
   end
 end
